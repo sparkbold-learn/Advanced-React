8aaf93e15b526455de2decb335485687273a255d
Trung Hoang <trunghoang.insperity@gmail.com>
2021-11-26 17:31:41 -0600

    Add custom MyApp with nprogress bar

---
+++ sick-fits/frontend/pages/_app.js
@@ -0,0 +1,21 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import NProgress from 'nprogress';
+import Router from 'next/router';
+import Page from '../components/Page';
+
+import 'nprogress/nprogress.css';
+import '../components/styles/nprogress.css';
+
+// Add progress bar when changing route
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
+// eslint-disable-next-line react/prop-types
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <Page>
+      <Component {...pageProps} />
+    </Page>
+  );
+}
