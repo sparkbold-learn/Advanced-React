542d4d6079925a20edc520dccadab816e09a477f
Trung Hoang <trunghoang.insperity@gmail.com>
2021-12-04 16:15:06 -0600

    Backend - Add keystone config file

---
+++ sick-fits/backend/keystone.ts
@@ -0,0 +1,35 @@
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import { User } from './schemas/User';
+
+import 'dotenv/config';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360,
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+    // TODO: Add data seeding here
+  },
+  lists: createSchema({
+    // Schema items go in here
+    User,
+  }),
+  ui: {
+    // Change this for roles
+    isAccessAllowed: () => true,
+  },
+  // TODO: Add session values here
+});
